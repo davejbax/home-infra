8dc6b4ff5a9061283db712cc0106d204c13a1c5a
davejbax <davejbax@gmail.com>
2026-09-04 22:43:52 +0100

    test

--- workloads/index.ts
+++ workloads/index.ts
@@ -3,7 +3,7 @@ import * as k8s from "@pulumi/kubernetes";
 import { provider } from "./k8s";
 
 const apps = new k8s.core.v1.Namespace("apps", {
-    metadata: { name: "apps" },
+    metadata: { name: "apps-test" },
 }, { provider });
 
 export const appsNamespace = apps.metadata.name;
