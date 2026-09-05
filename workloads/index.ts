b7a2d63413fa089260123baefb298485d196dca0
davejbax <davejbax@gmail.com>
2026-09-05 14:30:11 +0100

    workloads: Add Akri

--- workloads/index.ts
+++ workloads/index.ts
@@ -2,8 +2,27 @@ import * as k8s from "@pulumi/kubernetes";
 
 import { provider } from "./k8s";
 
-const apps = new k8s.core.v1.Namespace("apps", {
-    metadata: { name: "apps" },
+new k8s.helm.v3.Chart("akri", {
+    chart: "akri-dev",
+    version: "0.14.0",
+    namespace: "akri",
+    fetchOpts:{
+        repo: "https://project-akri.github.io/akri",
+    },
+    values: {
+      udev: {
+        configuration: {
+          enabled: true,
+          discoveryDetails: {
+            udevRules: [
+              'DRIVERS=="cp210x"'
+            ],
+            groupRecursive: true,
+          }
+        },
+        discovery: {
+          enabled: true
+        }
+      }
+    }
 }, { provider });
-
-export const appsNamespace = apps.metadata.name;
